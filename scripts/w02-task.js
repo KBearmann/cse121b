8ec16030d3be4142028c10dce16affb4e5e50d55
KBearmann <keillorbehrmann@gmail.com>
2023-11-11 17:43:12 -0700

    addedIMG

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -3,17 +3,28 @@
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
+let fullName = 'Keillor Behrmann';
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
+const profilePicture = 'images/IMG_Keillor1';
 
 
 
 
 /* Step 3 - Element Variables */
-
+const nameElement = document.getElementById ('name');
+const foodElement = document.getElementById ('food');
+const yearElement = document.querySelector('#year');
+const imageElement = document.querySelector('img');
 
 
 
 
 /* Step 4 - Adding Content */
+nameElement.innerHTML = '<strong>${fullName}</strong>';
+yearElement.textContent = '${currentYear}';
+imageElement.src = profilePicture;
+imageElement.alt = 'Profile image of ${fullName}';
 
 
 
@@ -21,7 +32,16 @@
 
 
 /* Step 5 - Array */
+const favoriteFood = ["Bbq","Tacos","Rolls","Steak and Potatoes"];
 
+foodElement.innerHTML = '<p>${favoriteFood}</p>';
+const faFood = "cookes";
+favoriteFood.push (faFood);
+foodElement.innerHTML += '<br>${favoriteFood}';
+favoriteFood.shift ();
+foodElement.innerHTML += '<br>${favoriteFood}';
+favoriteFood.pop ();
+foodElement.innerHTML += '<br>${favoriteFood}';
 
 
 
